ff0f0646a34097d916b8721e5b64bc4332203098
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 00:23:35 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/vinvalidator/main.go
@@ -0,0 +1,23 @@
+// cmd/vinvalidator/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "vinvalidator/internal/vinvalidator"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := vinvalidator.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
